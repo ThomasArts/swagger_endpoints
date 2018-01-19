fdbfb74bee5630b260dabe61628de2834ee72e2d
Thomas Arts <thomas.arts@quviq.com>
2018-01-19 14:44:22 +0100

    Source and Destination configurable

--- src/swagger_endpoints_prv.erl
+++ src/swagger_endpoints_prv.erl
@@ -17,8 +17,9 @@ init(State) ->
             {deps, ?DEPS},                % The list of dependencies
             {example, "rebar3 swagger_endpoints swagger.yaml"}, 
                                           % How to use the plugin
-            {opts, [{swaggerfile, $f, "file", string, "filename of swagger.yaml file"}]},
-                                                % list of options understood by the plugin
+            {opts, [{swaggerfile, $f, "file", string, "filename of swagger.yaml file"},
+                    {outfile, $o, "out", string, "filename for generated code (e.g. endpoints.erl)"}
+                   ]},                    % list of options understood by the plugin
             {short_desc, "Generate endpoints code from swagger"},
             {desc, "A rebar plugin to generate code to access swagger defined endpoints"}
     ]),
@@ -28,23 +29,32 @@ init(State) ->
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
     {KVs, _} = rebar_state:command_parsed_args(State),
-    case proplists:get_value(swaggerfile, KVs, 
-                             rebar_state:get(State, swagger_endpoints, undefined)) of
+    Source = proplists:get_value(swaggerfile, KVs, get_from_state(src, State, undefined)),
+    DefaultDest = 
+       case Source of
+           undefined -> undefined;
+           _ -> filename:basename(Source, [".yaml"]) ++ "_endpoints"
+      end,
+    Dest =  filename:rootname(
+              proplists:get_value(outfile, KVs, get_from_state(dst, State, DefaultDest)), 
+              ".erl") ++ ".erl",
+    case Source of
         undefined ->
-            {error, "Provide swagger file"};
+            {error, "Provide swagger file using option --file Filename"};
         Path ->
-            try Yaml = yamerl_constr:file(Path),
-                rebar_api:info("Generating code from ~p\n", [Path]),
-                 io:format("Got yaml: ~p\n", [Yaml]),
-                 {ok, State}
+            try YamlDocs = yamerl_constr:file(Path),
+                rebar_api:info("Generating code from ~p writing to ~p\n", [Path, Dest]),
+                {ok, State}
             catch
                 _:_ ->
                     {error, io_lib:format("Failed to parse ~p", [Path])}
             end
     end.
-   
-
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
+
+get_from_state(Key, State, Default) ->
+    KVs = rebar_state:get(State, swagger_endpoints, []),
+    proplists:get_value(Key, KVs, Default).
